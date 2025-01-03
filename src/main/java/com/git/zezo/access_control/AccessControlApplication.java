2f8e2ce0c4faff96ddf093cbef58e81b51398473
Fabio Kruschelski <bmx.fabio@gmail.com>
2025-01-03 10:02:33 -0300

    update project base

---
+++ src/main/java/com/git/zezo/access_control/AccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.git.zezo.access_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlApplication.class, args);
+	}
+
+}
